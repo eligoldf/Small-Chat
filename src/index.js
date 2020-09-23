d11e84cceecc7907997ebb6bb505b411c1e13c9e
eligoldf <eli.goldfdr@gmail.com>
2020-09-23 23:41:40 +0300

    add rollbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+import Rollbar from 'rollbar';
 import init from './init';
 import './locales/i18n';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
+} else {
+  // eslint-disable-next-line no-unused-vars
+  const rollbar = new Rollbar({
+    accessToken: '48b7f0bfa52142aca2ca7effbf552ab9',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  });
 }
 
 init();
