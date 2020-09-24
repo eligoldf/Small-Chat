2945844eb498c08ec0b0c79568e64977893f0f9b
eligoldf <eli.goldfdr@gmail.com>
2020-09-24 18:24:43 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-import Rollbar from 'rollbar';
 import init from './init';
 import './locales/i18n';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
-} else {
-  // eslint-disable-next-line no-unused-vars
-  const rollbar = new Rollbar({
-    accessToken: '20f3491c65a54154897a139de3c41ebd',
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    payload: {
-      environment: 'production',
-    },
-  });
 }
 
 init();
